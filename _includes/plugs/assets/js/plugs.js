c136978e562fb68cf50d160befd4a3d4432638dc
Meagan Truglio <257869376+meagantroot@users.noreply.github.com>
2026-04-22 00:56:03 -0700

    send it

--- _includes/plugs/assets/js/plugs.js
+++ _includes/plugs/assets/js/plugs.js
@@ -80,10 +80,10 @@ function stripSVG(svgDoc) {
 // --- Init ---
 async function init() {
     try {
-        const res = await fetch('{{ "/assets/plugs.json" | relative_url }}');
+        const res = await fetch('{{ "plugs/assets/plugs.json" | relative_url }}');
         db = await res.json();
 
-        const svgRes = await fetch('{{ "/assets/world.svg" | relative_url }}');
+        const svgRes = await fetch('{{ "plugs/assets/world.svg" | relative_url }}');
         const svgText = await svgRes.text();
 
         const parser = new DOMParser();
